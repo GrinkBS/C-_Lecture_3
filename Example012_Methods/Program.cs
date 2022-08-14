0846703b5d2b96cc620657a885d395dafd9d5335
Vyacheslav <grinksic@gmail.com>
2022-08-15 00:29:10 +0300

    lecture #3 is done

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -1,2 +1,138 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Вид 1 - ничего не возвращают и ничего не принимают
+
+// void Method1()
+// {
+//     Console.WriteLine("Автор ...");
+// }
+// Method1(); // Вызов метода. Без скобок будет ошибка
+
+// Вид 2 - ничего не возвращают, но могут принимать аргументы
+// void Method2(string msg)
+// {
+//     Console.WriteLine(msg);
+// }
+// Method2("Текст сообщения");
+
+// void Method21(string msg, int count)
+// {
+//     int i = 0;
+//     while (i<count)
+//     {
+//         Console.WriteLine(msg);
+//         i++;
+//     }
+// }
+// // Method21("Текст", count: 4);
+// Method21(count: 4,msg: "новый текст");
+
+//Вид 3 - что-то возвращают, но ичего не принимаюь
+
+// int Method3()
+// {
+//      return DateTime.Now.Year;
+// }
+// int year = Method3();
+// Console.WriteLine(year);
+
+// Вид4 - что-топринимают и что-то возвращают
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i<count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+// return result;
+// // }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+
+//         {
+//             result = result + text;
+//         }
+//     return result;
+// }
+
+// string res = Method4(10, "z");
+// Console.WriteLine(res);
+
+// for (int i = 2; i<= 10; i++)
+// {
+//     for (int j = 2; j <=10; j++)
+//     {
+//         Console.WriteLine($"{i} x {j}= {i * j}");
+//     }
+//     Console.WriteLine();
+// }
+
+
+// Работы с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы "к" заменить большими "К",
+// а большие "С" заменить маленьки "с"
+
+// string text = "- фывыфвыфв фывыфвф фывыфывыфвыфв фвыфвыфввфыы - фывфыфв"
+// + "фывфывфывыфв фывыфвыфв кпкппенро гогого";
+
+// // string s = "qwerty"
+// //             012
+// // s[3] // r
+// string Replace(string text, char oldValue,char newValue)
+// {
+//     string result = String .Empty;
+
+//     int length = text.Length;
+//     for (int i = 0; i < length; i++)
+//     {
+//         if(text[i] == oldValue) result = result + $"{newValue}";
+//         else result = result + $"{text[i]}";
+//     }
+
+//     return result;
+// }
+
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
